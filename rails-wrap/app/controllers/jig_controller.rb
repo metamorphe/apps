18ab37cbd7594d307aae1f9dbd42a69e6eefe47a
Cesar Torres <cearto@berkeley.edu>
2015-07-31 17:26:54 -0700

    collections

--- rails-wrap/app/controllers/jig_controller.rb
+++ rails-wrap/app/controllers/jig_controller.rb
@@ -14,13 +14,16 @@ class JigController < ApplicationController
   def interface
     @design = Design.find(params[:id]).to_json.html_safe
     @files = get_primitives()
-  	render :layout => "full_screen"
+    render :layout => "full_screen"
+  	# render :json => @files
   end
 
 
 
   # HELPER METHODS
   def get_primitives
-      {path: "/primitives/", filenames: Dir.glob("public/primitives/*").collect!{|c| c.split('/')[2..-1].join('/')}}
+    files = {path: "/primitives/", filenames: Dir.glob("public/primitives/*").collect!{|c| c.split('/')[2..-1].join('/')}}
+    files[:filenames].collect!{|f| {:collection => f.split('.')[0].split('-')[0].split('_')[0].titlecase, :filename => f, :title => f.split(".")[0].titlecase}}
+    files
   end
 end
