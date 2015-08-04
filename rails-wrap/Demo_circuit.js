43549d16dc86a28975c66a1c573411429d884592
Joanne Lo <jolo@adobe.com>
2015-08-04 11:44:16 -0700

    calculate circuit with info on traces

--- rails-wrap/Demo_circuit.js
+++ rails-wrap/Demo_circuit.js
@@ -1,10 +1,15 @@
 //demo-circuit
-            var demo_resistor = {resistance: 100};
-            var demo_resistor_pos = {resistance: 100};
-            var demo_battery = {voltage: 15};
+            var demo_battery_1 = {voltage: 15}; //this should blow up the LED
+            var demo_battery_2 = {voltage: 1}; //this should not turn on the LED
+            var demo_battery_3 = {voltage:5}; //this should be juuuust right 
 
-             function calculateResistanceFromLength(material, area){
-                return {resistance: material.conductance/area};
+            var demo_trace_pos = {tracelength: 400, tracewidth: 2}; 
+            var demo_trace_neg = {tracelength: 500, tracewidth: 2};
+            var demo_material = {name: "copper", sheetresistance: 0.5};
+
+
+            function calculateResistanceFromLength(material, trace){ //assuming the trace is uniform for now
+                return {resistance: material.sheetresistance*trace.tracelength/trace.tracewidth};
             }
 
             var demo_led = {
@@ -12,6 +17,9 @@
                 turnonvoltage: 2, 
                 brightness_max: 200 
             }
+
+            var demo_resistance_pos = calculateResistanceFromLength(demo_material.copper, demo_trace_pos); //100 ohm
+            var demo_resistance_neg = calculateResistanceFromLength(demo_material.copper, demo_trace_neg); //125 ohm
 
             function calculateLEDState(led, resistorePos, resistorNeg,  battery) {
                 var led_brightness = 0; // led_brightness varies from 0 to 1. set to -1 if it's blowing up.
@@ -27,12 +35,9 @@
                 }
                 
                 console.log("this is the led brightness", led_brightness);
-            return led_brightness;    
+            return led_brightness;     
             }
 
-            var demo_material = {name: "silver", resistance: 2}
-
-           
-
-
-            calculateLEDState(demo_led, demo_resistor, demo_resistor2, demo_battery);
+            calculateLEDState(demo_led, deom_resistance_pos, demo_resistance_neg, demo_battery_1);
+            calculateLEDState(demo_led, deom_resistance_pos, demo_resistance_neg, demo_battery_2);
+            calculateLEDState(demo_led, deom_resistance_pos, demo_resistance_neg, demo_battery_3);
