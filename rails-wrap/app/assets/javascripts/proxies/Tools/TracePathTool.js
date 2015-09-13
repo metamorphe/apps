4c7c891972f2ae7a5626e79f0a5c66f417e58d5c
Cesar Torres <cearto@berkeley.edu>
2015-09-13 16:34:37 -0700

    Path originating trace intersection test complete

--- rails-wrap/app/assets/javascripts/proxies/Tools/TracePathTool.js
+++ rails-wrap/app/assets/javascripts/proxies/Tools/TracePathTool.js
@@ -129,15 +129,14 @@ TracePathTool.prototype = {
 	    		start_terminal = null;
 	    		return;
 	    	}
-	    	// Are all connections of the same polarity
+	    // Are all connections of the same polarity
 	    	var offending_elements = [];
-	    	var valid_connection = _.reduce(intersects, function(memo, el, i, arr){
-	    		if(el.name == "trace")
-		    		var valid = start_terminal.style.fillColor.equals(el.style.strokeColor);
-		    	else
-		    		var valid = start_terminal.style.fillColor.equals(el.style.fillColor);
-	    		if(!valid) offending_elements.push(el);
-	    		return memo && valid;
+	    	var polarity = start_terminal.name == "trace" ? start_terminal.style.strokeColor : start_terminal.style.fillColor;
+	    		var valid_connection = _.reduce(intersects, function(memo, el, i, arr){
+	    			var el_polarity = el.name == "trace" ? el.style.strokeColor : el.style.fillColor;
+	    			var valid = polarity.equals(el_polarity);
+	    			if(!valid) offending_elements.push(el);
+	    			return memo && valid;
 	    	}, true);
 
 	    	if(valid_connection){
@@ -204,29 +203,116 @@ TracePathTool.prototype = {
 	},
 	trace: {
 		onMouseDown: function(event, hitResult, scope){
-			// var path = hitResult.item;
-			// start_trace = path;
+	  		var path = hitResult.item;
+			start_terminal = path;
 			
-			// trace = new paper.Path({
-			// 	strokeColor: path.style.strokeColor,
-			// 	strokeWidth: 4,
-			// 	name: "trace"
-			// });
-	  //   	trace.add(event.point);
+			trace = new paper.Path({
+				strokeColor: path.style.strokeColor,
+				strokeWidth: 4,
+				name: "trace"
+			});
+
+	    	trace.add(event.point);
 		}, 
 		onMouseDrag: function(event){
-			// trace.add(event.point);
+			trace.add(event.point);
 		}, 
 		onMouseUp: function(event){
-			// var terminals = designer.circuit_layer.getAllTerminals();
-			// var traces = designer.traces_layer.getAllTraces();
+   			// Get all conductive elements on the board
+			var terminals = designer.circuit_layer.getAllTerminals();
+			var traces = designer.traces_layer.getAllTraces();
+			var conductive = _.flatten([terminals, traces]);
 
 
+			// Find all unique non-self referential intersections with those elements
+	    	var intersects = TracePathTool.getAllIntersections(trace, conductive);
+			intersects = _.uniq(intersects);
+	    	intersects = _.reject(intersects, function(el, i, arr){
+	    		return el.parent.canvasItem.id == start_terminal.parent.canvasItem.id;
+	    	});
+
+	    	// Visual characteristics on MouseUp			
+			_.each(terminals, function(el, i, arr){
+				el.scaling = new paper.Point(1.0, 1.0);	
+			});
 			
 
-			// trace.simplify();
-			// trace.remove();
-   //  		designer.traces_layer.add(trace);
+
+	    	// Handle intersections
+	    	// Hanging trace
+	    	if(intersects.length == 0){
+	    		start_terminal = null;
+	    		return;
+	    	}
+	    	// Are all connections of the same polarity
+	    	var offending_elements = [];
+	    	var polarity = start_terminal.name == "trace" ? start_terminal.style.strokeColor : start_terminal.style.fillColor;
+	    		var valid_connection = _.reduce(intersects, function(memo, el, i, arr){
+	    			var el_polarity = el.name == "trace" ? el.style.strokeColor : el.style.fillColor;
+	    			var valid = polarity.equals(el_polarity);
+	    			if(!valid) offending_elements.push(el);
+	    			return memo && valid;
+	    	}, true);
+
+	    	if(valid_connection){
+	    		trace.simplify();
+	    		trace.remove();
+	    		designer.traces_layer.add(trace);
+	    	}
+	    	else{
+	    		// error message
+	    		trace.simplify();
+
+	    		var animations = [];
+    			_.each(offending_elements, function(el, i, arr){
+					console.log("Strobing", el.name);
+					el.style = {
+						shadowColor: "blue",
+						shadowBlur: 30,
+						shadowOffset: new paper.Point(0, 0)
+					}
+					animations.push(designer.animation_handler.add(function(event){
+						var t = Math.sin(event.count/5); //[-1, 1]
+						t += 1; //[0, 2];
+						t /= 2; //[0, 1];
+						el.shadowColor.alpha = t;
+					}, 1.5,
+					function(){
+						el.shadowColor.alpha = 0;
+					}));
+    			});
+
+	    		alerter.alert(TracePathTool.SHORT_MESSAGE,
+		    		function(){
+		    			trace.style = {
+							shadowColor: "blue",
+							shadowBlur: 30,
+							shadowOffset: new paper.Point(0, 0)
+						}
+
+						designer.animation_handler.add(function(event){
+							var t = Math.sin(event.count/5); //[-1, 1]
+							t += 1; //[0, 2];
+							t /= 2; //[0, 1];
+							trace.shadowColor.alpha = t;
+						}, 1.5,
+						function(){
+							_.each(animations, function(el, i, arr){
+								designer.animation_handler.remove(el);
+							});
+							if(trace) trace.remove();
+							trace.shadowColor.alpha = 0;
+						});
+						
+					},
+		    		"Remove the shorting path"
+	    		);
+	    		
+	    	}
+
+
+	    	// State variable update
+			start_terminal = null;
 		}
 	},
 	led: {
