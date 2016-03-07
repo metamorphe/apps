acfc9bdbfe2ab43010580b4cf0332b5d28508f51
Cesar Torres <cearto@berkeley.edu>
2016-03-07 14:19:56 -0800

    new instructions

--- rails-wrap/app/assets/javascripts/circuitree/FabGuide.js
+++ rails-wrap/app/assets/javascripts/circuitree/FabGuide.js
@@ -44,7 +44,7 @@ FabGuide.prototype = {
 					else
 						return memo;
 				}, 0);
-				console.log("FB", nodes, led, in_path);
+				// console.log("FB", nodes, led, in_path);
 				is_connected = is_connected || in_path >= 2;
 				if(is_connected){
 					match.push(j);
@@ -65,11 +65,21 @@ FabGuide.prototype = {
 			var dryingtime = Ruler.pts2mm(el.length);
 			dryingtime /= 10;
 			dryingtime = dryingtime.toFixed(0);
+			dryingtime = 60;
+			ohmage = sr_model.apply(el.solution);
+			
+
+			ohmage_upperbound = 2 * ohmage; 
+			ohmage_upperbound = ohmage_upperbound.toFixed(0);
+			led_terminals = scope.leds[i];
+			console.log(led_terminals);
 			var guide = [
 				{level: 1, elements: [el.solution.id], message: "Draw Trace #" + (i+ 1) + " with your silver ink pen."},
-				{level: 2, elements: [el.solution.id], message: "Let these traces dry for "+ dryingtime +" seconds."},
+				{level: 2, elements: [el.solution.id], message: "Let these traces dry for "+ dryingtime +" seconds.", time: dryingtime},
+				{level: 3, probeA: el.nodes[0], probeB: led_terminals[0],  elements: [el.solution.id], message: "Check the resistance of this trace. If the multimeter reads greater than " + ohmage_upperbound + " Ω, then let it dry a bit longer."},
+				{level: 3, probeA: led_terminals[1], probeB: el.nodes[el.nodes.length -1],  elements: [el.solution.id], message: "Check the resistance of this trace. If the multimeter reads greater than " + ohmage_upperbound + " Ω, then let it dry a bit longer."},
 				{level: 2, elements: [scope.battery.id], message: "Add the battery."},
-				{level: 2, elements: [el.solution.id], message: "With your multimeter, check if the line is powered."},
+				{level: 2, probeA: led_terminals[0], probeB: led_terminals[1], elements: [el.solution.id], message: "With your multimeter, check if the line is powered (voltage setting). Position the probes on the dots."},
 				{level: 2, elements: [scope.ledIDs[scope.match[i]]], message: "Place the following LED."},
 				{level: 3, elements: [scope.ledIDs[scope.match[i]]], message: "Does the LED turn on?"},
 				{level: 2, elements: [scope.battery.id], message: "Remove the battery."}	
@@ -79,24 +89,61 @@ FabGuide.prototype = {
 		guides = _.flatten(guides);
 
 		_.each(guides, function(el, i, arr){
-				console.log(el);
+				// console.log(el);
 				// var level = el.level == 1 ? "active" : "warning";
 				if(el.level == 1) level = "active";
 				if(el.level == 2) level = "list-group-item";
 				if(el.level == 3) level = "list-group-item-warning";
 		    	var guide_dom = $('<li class="list-group-item '+ level +'">'+ el.message +'</li>');
 		    	guide_dom.attr('data-highlight', el.elements.join(','));
+		    	if(el.probeA)
+			    	guide_dom.attr('data-probe-a', el.probeA);
+			    if(el.probeB)
+			    	guide_dom.attr('data-probe-b', el.probeB);
+		    	
+
 		    	guide_dom.hover(
 		    		function(){
 			    		elements = $(this).attr('data-highlight').split(',');
-			    		// console.log(elements);
+			    		probeA = parseInt($(this).attr('data-probe-a'));
+			    		probeB = parseInt($(this).attr('data-probe-b'));
+			    		// console.log(probeA);
 			    		_.each(elements, function(el, i, arr){
 			    			var el = Node.get(parseInt(el));
+			    			if(!_.isNaN(probeA)){
+			    				// probe plus
+			    				node = _.filter(graph.nodes, function(el, i, arr){ return el.id == probeA})[0];
+			    				node.enable();
+			    				node.self.style.fillColor = "red";
+			    				node.self.bringToFront();
+			    				// paper.project.addChild(node.clone())
+			    			}
+			    			if(!_.isNaN(probeB)){
+			    				// probe plus
+			    				node = _.filter(graph.nodes, function(el, i, arr){ return el.id == probeB})[0];
+			    				node.enable();
+			    				node.self.style.fillColor = "black";
+			    				node.self.bringToFront();
+
+			    			}
+			    	
+			    			// if(!_.isUndefined(el.probeA)) console.log("PROBE", el.probeA);
 			    			OhmTool.glow(el, true);
 			    		});
 			    	}, 
 			    	function(){
 			    		elements = $(this).attr('data-highlight').split(',');
+		    			if(!_.isNaN(probeA)){
+			    				// probe plus
+			    				node = _.filter(graph.nodes, function(el, i, arr){ return el.id == probeA})[0];
+			    				node.disable();
+			    		}
+			    		if(!_.isNaN(probeB)){
+			    				// probe plus
+			    				node = _.filter(graph.nodes, function(el, i, arr){ return el.id == probeB})[0];
+			    				node.disable();
+			    				node.self.style.fillColor = "black";
+			    			}
 			    		// console.log(elements);
 			    		_.each(elements, function(el, i, arr){
 			    			var el = Node.get(parseInt(el));
