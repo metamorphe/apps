d86fa9aa850fd00381cc8c157bfa138d1d754775
Cesar Torres <cearto@berkeley.edu>
2016-12-26 15:50:02 -0700

    tactile generator file specific

--- rails-apps/app/controllers/apps_controller.rb
+++ rails-apps/app/controllers/apps_controller.rb
@@ -1,6 +1,7 @@
 class AppsController < ApplicationController
   def tactile_generator
   	@digital_model = DigitalModel.find(params[:id])
+  	# render :json => @digital_model
   	render :layout => "full_screen"
   end
   def light_as_material
